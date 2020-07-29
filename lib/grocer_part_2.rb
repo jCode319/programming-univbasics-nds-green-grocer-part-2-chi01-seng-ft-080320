98596361740ed4006ce5334dcf1ac72e1efa37ee
Jarrell Jackson <jdjack3@gmail.com>
2020-07-29 04:31:24 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -17,7 +17,7 @@ def apply_coupons(cart, coupons)
         cart_item_with_coupon = {
           :item => coupon_item_name,
           :price => coupons[i][:cost] / coupons[i][:num],
-          :count => coupon[si][:num],
+          :count => coupons[i][:num],
           :clearance => cart_item[:clearance]
         }
           cart << cart_item_with_coupon
